df8e5ce2eca47baa4160cb580ae3b2eac2c423ac
aayushdoesdev <154404025+aayushdoesdev@users.noreply.github.com>
2024-03-06 13:00:28 +0530

    Update page.tsx

--- client/app/saved-recipes/page.tsx
+++ client/app/saved-recipes/page.tsx
@@ -13,7 +13,7 @@ import axios from "axios";
 import { useGetUserID } from "@/hooks/useGetUserID";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-
+axios.defaults.withCredentials = true;
 
 const page = () => {
   const [savedRecipes, setSavedRecipes] = useState([]);
@@ -23,7 +23,7 @@ const page = () => {
     const fetchSavedRecipes = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:4000/recipes/savedRecipes/${userID}`
+          `https://recipe-app-mern-six.vercel.app/recipes/savedRecipes/${userID}`
         );
         setSavedRecipes(response.data.savedRecipes);
       } catch (err) {
